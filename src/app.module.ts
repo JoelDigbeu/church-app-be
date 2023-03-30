891c3d738cfae5a1e2d7bcc53882026a83c21eca
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2023-03-30 12:15:43 +0000

    add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { AppController } from './app.controller'
 import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { NODE_ENV } from './shared'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === NODE_ENV.PRODUCTION
+          ? '.env'
+          : `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/common/config.ts
@@ -0,0 +1,4 @@
+export enum NODE_ENV {
+  PRODUCTION = 'prod',
+  DEVELOPMENT = 'dev',
+}
