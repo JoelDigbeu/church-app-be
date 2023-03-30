9a5d8b8ae656eaf88cbfa70fb0165609e639ce4f
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2023-03-30 15:14:03 +0000

    feat(prisma/shcema): add user model base property

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { PrismaService } from '@shared/prisma'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const prismaService = app.get<PrismaService>(PrismaService)
+  await prismaService.appShutdownHooks(app)
+
   await app.listen(3000)
 }
 bootstrap()
